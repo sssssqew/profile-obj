7db0c957de57c0ee2c2b56b3fbfb516c6961578a
sssssqew <sssssqew@naver.com>
2021-07-06 15:17:01 +0900

    refactoring: made buildElement helper function & applied and build card component

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,19 @@
+import buildElement from '../lib/helpers.js';
+
 // 이벤트 핸들러 정의
 function hideProfileImg(event) {
   event.target.classList.remove("show-profileImg");
 }
 
 // 컴포넌트 생성
-const cardPictureImg = document.createElement("img");
-cardPictureImg.className = "card-picture-img";
-cardPictureImg.src = "";
-cardPictureImg.alt = "";
+const cardPictureImg = buildElement('img', {className: 'card-picture-img', src: '', alt: ''})
+const cardPicture = buildElement('div', {id: 'card-picture'});
+cardPicture.appendChild(cardPictureImg);
 
-const cardPicture = document.createElement('div');
-cardPicture.id = "card-picture";
-cardPicture.appendChild(cardPictureImg)
-
-const cardName = document.createElement('div');
-cardName.id = "card-name";
-
-const cardComponent = document.createElement('div');
-cardComponent.id = "card-component";
-
-cardComponent.append(cardPicture, cardName);
+const cardName = buildElement('div', {id: 'card-name'});
+const cardComponent = buildElement('div', {id: 'card-component'});
+cardComponent.appendChild(cardPicture);
+cardComponent.appendChild(cardName);
 
 const profileCardContainer = document.getElementById('profile-card-container');
 profileCardContainer.insertBefore(cardComponent, profileCardContainer.firstChild);
