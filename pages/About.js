9cac8b95a1fc21f3a448c0eaf34a3fff2de240c5
sssssqew <sssssqew@naver.com>
2021-07-09 14:18:14 +0900

    made About page boilerplate codes

---
+++ pages/About.js
@@ -0,0 +1,28 @@
+import buildElement from '../lib/helpers.js';
+
+function About(){
+  // 이벤트 핸들러 정의
+  
+  
+  // 컴포넌트 생성
+  function buildComponent(){
+    
+  }
+  // 이벤트 핸들러 연결
+  function attachHandlers(){
+  
+  }
+  // 하위 컴포넌트 생성
+  function addComponents(){
+   
+  }
+
+  function init(){
+    buildComponent();
+    attachHandlers();
+    addComponents();
+  }
+  init(); // 컴포넌트 생성 + 이벤트핸들러 연결
+}
+
+export default About;
