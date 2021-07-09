6c672824c5bc24846425843ba476ad5b92a12623
sssssqew <sssssqew@naver.com>
2021-07-09 13:17:49 +0900

    refactoring: made Home page and implemented boilerplate codes

---
+++ pages/Home.js
@@ -0,0 +1,24 @@
+import buildElement from '../lib/helpers.js';
+
+function Home(){
+  // 이벤트 핸들러 정의
+  
+  // 컴포넌트 생성
+  function buildComponent(){
+    
+  }
+  // 이벤트 핸들러 연결
+  function attachHandlers(){
+    
+  }
+  function addComponents(){}
+  
+  function init(){
+    buildComponent();
+    attachHandlers();
+    addComponents();
+  }
+  init(); // 컴포넌트 생성 + 이벤트핸들러 연결
+}
+
+export default Home;
