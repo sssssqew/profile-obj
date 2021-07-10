faeba6a3ddd04b9cb850ba4caaa6c3b07fbfcab5
sssssqew <sssssqew@naver.com>
2021-07-10 12:13:23 +0900

    refactoring: applied changing url address when navigating page

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import router from './lib/router.js';
 
+window.popped = false;
+
 router('/');
 
-
+window.addEventListener('popstate', function(event){
+  window.popped = true;
+  router(event.state);
+})
 
 
 
