d1fdeef388a14530230c0f105524ee4d3aa54dd2
sssssqew <sssssqew@naver.com>
2021-07-14 16:35:45 +0900

    refactoring: took out UserInfo Component

---
+++ components/UserInfo.js
@@ -0,0 +1,41 @@
+import {buildElement, updateElement, $} from '../lib/helpers.js';
+import router from '../lib/router.js';
+
+function UserInfo(){
+  // 이벤트 핸들러 정의
+  function hideProfileImg(event) {
+    updateElement(event.target.id, {'className': 'userInfo-profile-img'});
+  }
+  
+  // 컴포넌트 생성
+  function buildComponent(){
+    const userInfoData = JSON.parse(sessionStorage.getItem('userInfoData'));
+    const userInfoComponent = buildElement('div', {'id': 'userInfo-component'}, [
+      buildElement('div',{'id': 'userInfo-profile'}, [
+        buildElement('img', {'id': 'userInfo-profile-img', 'className': 'userInfo-profile-img', 'src': `${$(userInfoData).userProfileImg? userInfoData.userProfileImg: ''}`, 'alt': ''})
+      ]),
+      buildElement('div', {'id': 'userInfo-container'}, [
+        buildElement('div', {'id': 'userInfo-info'}, [
+          buildElement('p', {'id': 'profile-name'}, [$(userInfoData).userName? `Name: ${userInfoData.userName}`: '']),
+          buildElement('p', {'id': 'profile-age'}, [$(userInfoData).userAge? `Age: ${userInfoData.userAge}`: '']),
+          buildElement('p', {'id': 'profile-gender'}, [$(userInfoData).userGender? `Gender: ${userInfoData.userGender}`: ''])
+        ])
+      ])
+    ])
+
+    updateElement('profile-contents', {}, [userInfoComponent]);
+  }
+
+  // 이벤트 핸들러 연결
+  function attachHandlers(){
+    document.getElementById('userInfo-profile-img').addEventListener("error", hideProfileImg);
+  }
+  
+  function init(){
+    buildComponent();
+    attachHandlers();
+  }
+  init(); // 컴포넌트 생성 + 이벤트핸들러 연결
+}
+
+export default UserInfo;
--- pages/About.js
+++ pages/About.js
@@ -1,48 +1,32 @@
 import {buildElement, updateElement, $} from '../lib/helpers.js';
+
 import Nav from '../components/Nav.js';
+import UserInfo from '../components/UserInfo.js';
 
 function About(){
-  // 이벤트 핸들러 정의
-  function hideProfileImg(event) {
-    updateElement(event.target.id, {'className': 'userInfo-profile-img'});
-  }
   
   // 컴포넌트 생성
   function buildComponent(){
-    const userInfoData = JSON.parse(sessionStorage.getItem('userInfoData'));
     const aboutPage = buildElement('div', {'id': 'profile-about'}, [
       buildElement('div', {'id': 'profile-nav'}),
-      buildElement('div', {'id': 'profile-contents'}, [
-        buildElement('div', {'id': 'userInfo-component'}, [
-          buildElement('div',{'id': 'userInfo-profile'}, [
-            buildElement('img', {'id': 'userInfo-profile-img', 'className': 'userInfo-profile-img', 'src': `${$(userInfoData).userProfileImg? userInfoData.userProfileImg: ''}`, 'alt': ''})
-          ]),
-          buildElement('div', {'id': 'userInfo-container'}, [
-            buildElement('div', {'id': 'userInfo-info'}, [
-              buildElement('p', {'id': 'profile-name'}, [$(userInfoData).userName? `Name: ${userInfoData.userName}`: '']),
-              buildElement('p', {'id': 'profile-age'}, [$(userInfoData).userAge? `Age: ${userInfoData.userAge}`: '']),
-              buildElement('p', {'id': 'profile-gender'}, [$(userInfoData).userGender? `Gender: ${userInfoData.userGender}`: ''])
-            ])
-          ])
-        ])
-      ]),
+      buildElement('div', {'id': 'profile-contents'})
     ]);
 
     updateElement('root', {}, ['']);
     updateElement('root', {}, [aboutPage]);
   }
   // 이벤트 핸들러 연결
-  function attachHandlers(){
-    document.getElementById('userInfo-profile-img').addEventListener("error", hideProfileImg);
-  }
+  function attachHandlers(){}
+
   // 하위 컴포넌트 생성
   function addComponents(){
     Nav();
+    UserInfo();
   }
 
   function doSomethingAfterRendering(){
     const userInfoData = JSON.parse(sessionStorage.getItem('userInfoData'));
-    if(userInfoData && userInfoData.userProfileImg){
+    if($(userInfoData).userProfileImg){
       updateElement('userInfo-profile-img', {'className': 'userInfo-profile-img show-profileImg'}); // 프로필 사진 보여주기
     } 
   }
