d4e78e047f28d5aa425a27d3aa2f10e8aa83e821
sssssqew <sssssqew@naver.com>
2021-07-28 13:49:14 +0900

    implemented unit test for validation check of user inputs

--- test/modal-test.js
+++ test/modal-test.js
@@ -1,36 +1,39 @@
 import {test, assert} from '../lib/test.js';
 import {buildElement, updateElement, readElementProp, searchElement} from '../lib/helpers.js';
 
+// 모달창의 취소 버튼을 클릭했을때 모달창에 입력한 데이터를 삭제하고 모달창이 제대로 숨겨지는지 테스트
 const test1 = 'should clear and hide modal when clicked X button';
+// 모달창의 파일선택 버튼을 클릭했을때 파일 캐쉬를 제대로 삭제하는지 테스트
 const test2 = 'should clear file cash when clicked input button';
 
-// change 이벤트가 발생했을때 사용자가 선택한 파일이 없는 경우 빈값을 제대로 리턴하는지 테스트
+// change 이벤트가 발생했을때 사용자가 선택한 파일이 없는 경우 파일 객체 배열의 길이가 0인지 테스트
 const test3 = 'should return length of file 0 when user does not select file';
-
-// change 이벤트가 발생했을때 사용자가 선택한 파일 타입에 따라 boolean 값을 제대로 리턴하는지 테스트
-const test4 = 'should return true when user select valid file';
-const test5 = 'should return false when user select invalid file';
-
+// change 이벤트가 발생했을때 사용자가 선택한 파일 확장자가 validation 검사를 통과하는지 테스트
+const test4 = 'should pass validation check of file extension when user select file';
 // change 이벤트가 발생했을때 사용자가 선택한 파일 이름이 UI에 제대로 반영되는지 테스트
-const test6 = 'should update file name on UI when user select valid file';
+const test5 = 'should update file name on UI when user select valid file';
 // change 이벤트가 발생했을때 사용자가 선택한 파일 데이터를 제대로 로드하는지 테스트
-const test7 = 'should load file data when user select valid file';
+const test6 = 'should load file data when user select valid file';
 
-// save 버튼을 클릭했을때 사용자가 데이터를 입력하지 않거나 유효하지 않은 데이터를 입력한 경우 validation이 실패하는지 테스트
-const test8 = 'should return false of validation check when user provide invalid data'; 
 
+// save 버튼을 클릭했을때 사용자가 유효하지 않은 데이터를 입력한 경우 validation이 실패하는지 테스트
+const test7 = 'should return false of validation check when user provide invalid data'; 
 // save 버튼을 클릭했을때 사용자 정보를 세션 스토리지에 잘 저장하는지 테스트
-const test9 = 'should save user information on session storage when user click save button';
-
+const test8 = 'should save user information on session storage when user click save button';
 // save 버튼을 클릭했을때 프로필 사진이 화면에 잘 표시되는지 테스트
-const test10 = 'should display profile image on UI when user click save button';
-
+const test9 = 'should display profile image on UI when user click save button';
 // save 버튼을 클릭했을때 프로필 이름이 화면에 잘 표시되는지 테스트
-const test11 = 'should display profile name on UI when user click save button';
-
+const test10 = 'should display profile name on UI when user click save button';
 // save 버튼을 클릭했을때 알람 메세지가 화면에 잘 표시되는지 테스트
-const test12 = 'should display alert message on UI when user click save button';
+const test11 = 'should display alert message on UI when user click save button';
 
+/* 단위 테스트 (Unit test) */
+const test12 = 'should pass validation check of file extension for given test cases';
+const test13 = 'should pass validation check whether if string has special characters for given test cases';
+const test14 = 'should pass validation check whether if string has numbers for given test cases';
+const test15 = 'should pass validation check whether if string is only numbers for given test cases';
+const test16 = 'should pass validation check whether if string is valid age for given test cases';
+const test17 = 'should pass validation check whether if string is valid gender for given test cases';
 
 function createModalComponent(){
   const modalComponent = buildElement('div', {'id': 'profile-modal', 'className': 'profile-modal'}, [
@@ -69,6 +72,7 @@ function validateFileExtension(fileData) {
 function checkIfNumberIsInteger(num) {
   return num % 1 === 0;
 } // 현재는 사용하지 않음
+
 function checkIfStringHasSpecialCharacter(str) {
   const re = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
   return re.test(str);
@@ -212,58 +216,43 @@ function modalComponentTest(){
   })
 
   test(test4, ()=>{
-    document.body.appendChild(createModalComponent())
-    let isValidedFile = false;
+    const testCases = [
+      {file: [new File([""], "dummy.png", { type: 'image/png' })], exp: true},
+      {file: [new File([""], "dummy.pdf", { type: 'application/pdf' })], exp: false}
+    ]
+    
+    const result = testCases.every(function(c){
+      document.body.appendChild(createModalComponent())
+      let isValidedFile = false;
+      
+      function handlePictureUpload(event) {
+        // console.log('file is uploading on testing ...')
 
-    function handlePictureUpload(event) {
-      // console.log('file is uploading on testing ...')
-
-      const selectedFiles = [new File([""], "dummy.png", { type: 'image/png' })]; // 사용자가 직접 파일을 선택하지 못하므로 file 객체를 생성해서 사용함 (mocking)
-      // 파일 확장자 유효성 검사
-      if (!validateFileExtension(selectedFiles[0])) {
-        isValidedFile = false;
-      }else{
-        isValidedFile = true;
+        const selectedFiles = c.file; // 사용자가 직접 파일을 선택하지 못하므로 file 객체를 생성해서 사용함 (mocking)
+        // 파일 확장자 유효성 검사
+        if (!validateFileExtension(selectedFiles[0])) {
+          isValidedFile = false;
+        }else{
+          isValidedFile = true;
+        }
       }
-    }
-    searchElement("modal-file").addEventListener("change", handlePictureUpload);
+      searchElement("modal-file").addEventListener("change", handlePictureUpload);
 
-    const event = new Event('change');
-    searchElement('modal-file').dispatchEvent(event)
+      const event = new Event('change');
+      searchElement('modal-file').dispatchEvent(event)
+
+      document.body.removeChild(searchElement('profile-modal'))
+      return isValidedFile === c.exp;
+    })
 
     console.log(`\n[ ${test4} ]`)
-    assert(isValidedFile === true, 'uploaded file is valid successfully !', 'uploaded file is failed to valid !')
-    document.body.removeChild(searchElement('profile-modal'))
+    assert(result, 'passed validation check of file extension successfully !', 'failed to valid file extension !')
     console.log('\n')
+
+    
   })
 
   test(test5, ()=>{
-    document.body.appendChild(createModalComponent())
-    let isValidedFile = null;
-
-    function handlePictureUpload(event) {
-      // console.log('file is uploading on testing ...')
-
-      const selectedFiles = [new File([""], "dummy.pdf", { type: 'application/pdf' })]; // 사용자가 직접 파일을 선택하지 못하므로 file 객체를 생성해서 사용함 (mocking)
-      // 파일 확장자 유효성 검사
-      if (!validateFileExtension(selectedFiles[0])) {
-        isValidedFile = false;
-      }else{
-        isValidedFile = true;
-      }
-    }
-    searchElement("modal-file").addEventListener("change", handlePictureUpload);
-
-    const event = new Event('change');
-    searchElement('modal-file').dispatchEvent(event)
-
-    console.log(`\n[ ${test5} ]`)
-    assert(isValidedFile === false, 'uploaded file is not valid successfully !', 'uploaded file is failed to be invalid !')
-    document.body.removeChild(searchElement('profile-modal'))
-    console.log('\n')
-  })
-
-  test(test6, ()=>{
     document.body.appendChild(createModalComponent())
     
     function setUploadedFileName(fileData) {
@@ -280,13 +269,13 @@ function modalComponentTest(){
     const event = new Event('change');
     searchElement('modal-file').dispatchEvent(event)
 
-    console.log(`\n[ ${test6} ]`)
+    console.log(`\n[ ${test5} ]`)
     assert(readElementProp('modal-filename', 'innerText') === 'dummy.png', 'uploaded file name is updated on UI successfully !', 'uploaded file name is failed to be updated on UI !')
     document.body.removeChild(searchElement('profile-modal'))
     console.log('\n')
   })
 
-  test(test7, ()=>{
+  test(test6, ()=>{
     document.body.appendChild(createModalComponent())
     const state = {loadedPictureData: ''};
     const str = "I don't care about a broken image";
@@ -324,14 +313,14 @@ function modalComponentTest(){
     // 파일을 읽을때 어느정도 시간이 걸리고 onload 핸들러는 이벤트루프에 등록되어 맨 나중에 실행되기 때문에
     // 결과값 비교를 onload 핸들러 실행뒤에 하려면 결과값 비교 코드블럭을 setTimeout으로 묶어 onload 핸들러 이후에 이벤트루프에 등록해야 한다 (이렇게 하면 다음 테스트 이후에 실행되기 때문에 이렇게 하면 안된다)
    
-    console.log(`\n[ ${test7} ]`)
+    console.log(`\n[ ${test6} ]`)
     assert(state.loadedPictureData === imgData, 'loaded file data successfully !', 'failed to load file data !')
     document.body.removeChild(searchElement('profile-modal'))
     console.log('\n')
   
   })
 
-  test(test8, ()=>{
+  test(test7, ()=>{
     const testCases = [
       {name: '', age: '3', gender: 'female', fileName: "Test file name"},            // 사용자가 이름을 입력하지 않은 경우 
       {name: 'dummyname$', age: '3', gender: 'female', fileName: "Test file name"},  // 사용자가 입력한 이름에 특수문자가 포함된 경우 
@@ -370,12 +359,12 @@ function modalComponentTest(){
       return isValidUserInfo === false;
     })
 
-    console.log(`\n[ ${test8} ]`)
+    console.log(`\n[ ${test7} ]`)
     assert(result, 'returned false of validation check successfully !', 'failed to return false of validation check !')
     console.log('\n')
   })
 
-  test(test9, ()=>{
+  test(test8, ()=>{
     document.body.appendChild(createModalComponent())
     const str = "I don't care about a broken image";
     const state = {loadedPictureData: createImgData(str)}
@@ -399,13 +388,13 @@ function modalComponentTest(){
     const event = new Event('click');
     searchElement('modal-save').dispatchEvent(event)
 
-    console.log(`\n[ ${test9} ]`)
+    console.log(`\n[ ${test8} ]`)
     assert(sessionStorage.getItem('userInfoData'), 'saved user information successfully on session storage !', 'failed to save user information on session storage !')
     document.body.removeChild(searchElement('profile-modal'))
     console.log('\n')
   })
 
-  test(test10, ()=>{
+  test(test9, ()=>{
     document.body.appendChild(createModalComponent())
     const profileImg = buildElement('img', {'id': 'card-picture-img', 'src': '', 'className': 'card-picture-img'})
     document.body.appendChild(profileImg)
@@ -424,14 +413,14 @@ function modalComponentTest(){
     const event = new Event('click');
     searchElement('modal-save').dispatchEvent(event)
 
-    console.log(`\n[ ${test10} ]`)
+    console.log(`\n[ ${test9} ]`)
     assert( (readElementProp('card-picture-img', 'src') === state.loadedPictureData) && (readElementProp('card-picture-img', 'className') === 'card-picture-img show-profileImg'), 'displayed profile image on UI successfully !', 'failed to display profile image on UI !')
     document.body.removeChild(searchElement('profile-modal'))
     document.body.removeChild(profileImg)
     console.log('\n')
   })
 
-  test(test11, ()=>{
+  test(test10, ()=>{
     document.body.appendChild(createModalComponent())
     const cardName = buildElement('div', {'id': 'card-name'});
     document.body.appendChild(cardName)
@@ -454,14 +443,14 @@ function modalComponentTest(){
     const event = new Event('click');
     searchElement('modal-save').dispatchEvent(event)
 
-    console.log(`\n[ ${test11} ]`)
+    console.log(`\n[ ${test10} ]`)
     assert(readElementProp('card-name', 'innerText') === readElementProp("modal-info-name", 'value').trim(), 'displayed profile name on UI successfully !', 'failed to display profile name on UI !')
     document.body.removeChild(searchElement('profile-modal'))
     document.body.removeChild(cardName)
     console.log('\n')
   })
 
-  test(test12, ()=>{
+  test(test11, ()=>{
     document.body.appendChild(createModalComponent())
     const alertComponent = buildElement('div', {'id': 'alert-component', 'className': 'alert-component'}, [
       buildElement('div', {'id': 'alert-msg'}, ['']),
@@ -484,10 +473,165 @@ function modalComponentTest(){
     const event = new Event('click');
     searchElement('modal-save').dispatchEvent(event)
 
-    console.log(`\n[ ${test12} ]`)
+    console.log(`\n[ ${test11} ]`)
     assert( (readElementProp('alert-component', 'className') === 'alert-component show-alert') && (readElementProp('alert-msg', 'innerText') === 'dummy message'), 'displayed alert message on UI successfully !', 'failed to display alert message on UI !')
     document.body.removeChild(searchElement('profile-modal'))
     document.body.removeChild(alertComponent)
+    console.log('\n')
+  })
+
+  /* 단위 테스트 */
+
+  test(test12, () => {
+    const testCases = [
+      {file: new File([""], "test.pdf", { type: 'application/pdf' }), exp: undefined},
+      {file: new File([""], "test.mp4", { type: 'audio/mp4' }), exp: undefined},
+      {file: new File([""], "test.ttf", { type: 'font/ttf' }), exp: undefined},
+      {file: new File([""], "test.png", { type: 'image/png' }), exp: true},
+      {file: new File([""], "test.3mf", { type: 'model/3mf' }), exp: undefined},
+      {file: new File([""], "test.html", { type: 'text/html' }), exp: undefined},
+      {file: new File([""], "test.mp4", { type: 'video/mp4' }), exp: undefined},
+    ]
+
+    const result = testCases.every(function(c){
+      return validateFileExtension(c.file) === c.exp;
+    })
+
+    console.log(`\n[ ${test12} ]`)
+    assert(result, 'passed validation check of file extension successfully !', 'failed to pass validation check of file extension !')
+    console.log('\n')
+  })
+
+  test(test13, () => {
+    const testCases = [
+      {str: 'test', exp: false},
+      {str: 'test[', exp: true},
+      {str: 'test`', exp: true},
+      {str: 'test!', exp: true},
+      {str: 'test@', exp: true},
+      {str: 'test#', exp: true},
+      {str: 'test$', exp: true},
+      {str: 'test%', exp: true},
+      {str: 'test^', exp: true},
+      {str: 'test&', exp: true},
+      {str: 'test*', exp: true},
+      {str: 'test(', exp: true},
+      {str: 'test)', exp: true},
+      {str: 'test_', exp: true},
+      {str: 'test+', exp: true},
+      {str: 'test-', exp: true},
+      {str: 'test=', exp: true},
+      {str: 'test{', exp: true},
+      {str: 'test}', exp: true},
+      {str: 'test;', exp: true},
+      {str: "test'", exp: true},
+      {str: 'test:', exp: true},
+      {str: 'test"', exp: true},
+      {str: 'test|', exp: true},
+      {str: 'test,', exp: true},
+      {str: 'test.', exp: true},
+      {str: 'test<', exp: true},
+      {str: 'test>', exp: true},
+      {str: 'test/', exp: true},
+      {str: 'test?', exp: true},
+      {str: 'test~', exp: true},
+    ]
+
+    const result = testCases.every(function(c){
+      return checkIfStringHasSpecialCharacter(c.str) === c.exp;
+    })
+
+    console.log(`\n[ ${test13} ]`)
+    assert(result, 'passed validation check whether if string has special characters successfully !', 'failed to pass validation check whether if string has special characters !')
+    console.log('\n')
+  })
+
+  test(test14, () => {
+    const testCases = [
+      {str: 'test', exp: false},
+      {str: '123test', exp: true},
+      {str: 't123est', exp: true},
+      {str: 'te123st', exp: true},
+      {str: 'tes123t', exp: true},
+      {str: 'test123', exp: true}
+    ]
+
+    const result = testCases.every(function(c){
+      return checkIfStringHasNumbers(c.str) === c.exp;
+    })
+
+    console.log(`\n[ ${test14} ]`)
+    assert(result, 'passed validation check whether if string has numbers successfully !', 'failed to pass validation check whether if string has numbers !')
+    console.log('\n')
+  })
+
+  test(test15, () => {
+    const testCases = [
+      {str: 'test', exp: false},
+      {str: 'test123', exp: false},
+      {str: 'test@#$', exp: false},
+      {str: '1234@^&*', exp: false},
+      {str: '13.4545', exp: false},
+      {str: '1343535', exp: true}
+    ]
+
+    const result = testCases.every(function(c){
+      return checkIfStringIsOnlyNumbers(c.str) === c.exp;
+    })
+
+    console.log(`\n[ ${test15} ]`)
+    assert(result, 'passed validation check whether if string is only numbers successfully !', 'failed to pass validation check whether if string is only numbers !')
+    console.log('\n')
+  })
+
+  test(test16, () => {
+    const testCases = [
+      {str: 'test', exp: undefined},
+      {str: '13.4', exp: undefined},
+      {str: '-45', exp: undefined},
+      {str: '0', exp: undefined},
+      {str: '37', exp: true},
+      {str: '35%', exp: undefined},
+      {str: 'test37', exp: undefined},
+      {str: '257', exp: undefined}
+    ]
+
+    const result = testCases.every(function(c){
+      return checkIfAgeIsValid(c.str) === c.exp;
+    })
+
+    console.log(`\n[ ${test16} ]`)
+    assert(result, 'passed validation check whether if string is valid age successfully !', 'failed to pass validation check whether if string is valid age !')
+    console.log('\n')
+  })
+
+  test(test17, () => {
+    const testCases = [
+      {str: 'male', exp: true},
+      {str: 'female', exp: true},
+      {str: '', exp: undefined},
+      {str: 'mail', exp: undefined},
+      {str: 'mal', exp: undefined},
+      {str: 'mael', exp: undefined},
+      {str: 'meal', exp: undefined},
+      {str: 'femail', exp: undefined},
+      {str: 'femeal', exp: undefined},
+      {str: 'Feminine', exp: undefined},
+      {str: 'guy', exp: undefined},
+      {str: 'man', exp: undefined},
+      {str: 'mr', exp: undefined},
+      {str: 'girl', exp: undefined},
+      {str: 'woman', exp: undefined},
+      {str: 'mrs', exp: undefined},
+      {str: 'gender', exp: undefined}
+    ]
+
+    const result = testCases.every(function(c){
+      return checkIfGenderIsValid(c.str) === c.exp;
+    })
+
+    console.log(`\n[ ${test17} ]`)
+    assert(result, 'passed validation check whether if string is valid gender successfully !', 'failed to pass validation check whether if string is valid gender !')
     console.log('\n')
   })
 
